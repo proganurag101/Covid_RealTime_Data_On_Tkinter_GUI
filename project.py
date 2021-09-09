7fb5b131de9049994b6704009d64439e82168d25
proganurag101 <anurag94530@gmail.com>
2021-09-09 15:06:11 +0530

    2nd commit

--- project.py
+++ project.py
@@ -1,9 +1,11 @@
+
 # do all imports
 import requests
 import bs4
 import tkinter as tk
 import time
-import datetimec
+import datetime
+import selenium
 
 
 def get_html_data(url):
@@ -14,28 +16,42 @@ def get_html_data(url):
 def get_corona_detail_of_india():
     url = "https://www.worldometers.info/coronavirus/country/india/"
     html_data = get_html_data(url)
-
-    bs = bs4.BeautifulSoup(html_data.text, 'html.parser')
-    info_div1 = bs.find("div", class_="content-inner").find_all("div", id="maincounter-wrap")
-    l = list()
-    all_details = ""
-    for block in info_div1:
-        count = block.find("span").get_text()
-        text = block.find("h1").get_text()
-        all_details = all_details + text + " : " + count + "\n"
-
-    return (all_details)
+    bs = bs4.BeautifulSoup(html_data.text,'html.parser')
+    info_div = bs.find("div",class_="content-inner").find_all("div",id="maincounter-wrap")
+    all_data =""
+    for i in range(3):
+        text =  info_div[i].find("h1", class_ = None).get_text()
+        count = info_div[i].find("span", class_ = None).get_text()
+        all_data = all_data + text + " " + count + "\n"
+    return all_data
 
 
-# creating GUI
+def reload():
+    new_data = get_corona_detail_of_india()
+    mainlabel['text']=new_data
+
+
+get_corona_detail_of_india()
+
+#GUI
+
 root = tk.Tk()
-root.geometry("200x200")
-
+root.geometry("500x500")
 root.title("CORONA DATA TRACKER INDIA")
-f = ("poppins", 25, "bold")
-mainLabel = tk.Label(root, text=get_corona_detail_of_india())
-mainLabel.pack()
+f = ("poppins",25,"bold")
+
+banner = tk.PhotoImage(file="covid.png")
+bannerlabel = tk.Label(root,image=banner)
+bannerlabel.pack()
+
+
+
+mainlabel = tk.Label(root,text=get_corona_detail_of_india(),font=f)
+mainlabel.pack()
+
+
+rbtn = tk.Button(root,text='Reload',font=f,relief='solid',command=reload())
+rbtn.pack()
 
 root.mainloop()
 
-get_corona_detail_of_india()
